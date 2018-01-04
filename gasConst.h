b0a5a9445c4b8e0446b189aeb85ff2139639c836
ddydai <33012868+ddydai@users.noreply.github.com>
2018-01-04 18:08:41 +0800

    Add files via upload

---
+++ gasConst.h
@@ -0,0 +1,18 @@
+//gasConst.h 气体常数库
+// copyright by daiyuqiang@dlut.edu.cn
+
+
+#include <iostream>
+#include <math.h>
+
+const double Cpv=1.4;
+const double Cpv1 =1.4;
+const double Cpv2=1.4;
+
+//const double Cpv=0.9+sqrt(3.)/2.;
+const double Rg =287.1;
+const double Rg1=287.1;
+const double Rg2=287.1;
+
+const double PI =3.14159;
+const double EPS=1e-6;
